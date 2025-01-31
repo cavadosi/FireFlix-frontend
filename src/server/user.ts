971091da2d298a84054d7f2d9c617e03d3219374
cavadosi <jpeque.moab@gmail.com>
2025-01-31 15:03:10 +0100

    Add Missing enpoints to server/user

--- src/server/user.ts
+++ src/server/user.ts
@@ -24,9 +24,13 @@ const GetRequestToken = async (): Promise<ApiResponse<string>> => {
   }
 };
 
-const GetSessionId = async (requestToken: string): Promise<ApiResponse<string>> => {
+const GetSessionId = async (
+  requestToken: string
+): Promise<ApiResponse<string>> => {
   try {
-    const response = await fetch(`${baseUrl}/Account/session/new/${requestToken}`);
+    const response = await fetch(
+      `${baseUrl}/Account/session/new/${requestToken}`
+    );
     const data = await response.json();
 
     if (!response.ok) {
@@ -46,7 +50,9 @@ const GetSessionId = async (requestToken: string): Promise<ApiResponse<string>> 
   }
 };
 
-const GetAccountDetails = async (sessionId: string): Promise<ApiResponse<User>> => {
+const GetAccountDetails = async (
+  sessionId: string
+): Promise<ApiResponse<User>> => {
   try {
     const response = await fetch(`${baseUrl}/Account/details/${sessionId}`);
     const data = await response.json();
@@ -68,26 +74,110 @@ const GetAccountDetails = async (sessionId: string): Promise<ApiResponse<User>> 
   }
 };
 
-const GetUserList = async (query: string, id: string):Promise<ApiResponse<MediaList>> => {
-    try {
-        const response = await fetch(`${baseUrl}/Account/${query}/${id}`);
-        const data = await response.json();
-    
-        if (!response.ok) {
-          return {
-            status: response.status,
-            error: `HTTP error! Status: ${response.status}`,
-          };
-        }
-    
-        return { status: 200, data };
-      } catch (error) {
-        console.error("Error:", error);
-        return {
-          status: 400,
-          error: error instanceof Error ? error.message : "Unknown error",
-        };
-      }
-}
+const GetUserList = async (
+  query: string,
+  id: string
+): Promise<ApiResponse<MediaList>> => {
+  try {
+    const response = await fetch(`${baseUrl}/Account/${query}/${id}`);
+    const data = await response.json();
 
-export default { GetRequestToken, GetSessionId, GetAccountDetails, GetUserList };
+    if (!response.ok) {
+      return {
+        status: response.status,
+        error: `HTTP error! Status: ${response.status}`,
+      };
+    }
+
+    return { status: 200, data };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      status: 400,
+      error: error instanceof Error ? error.message : "Unknown error",
+    };
+  }
+};
+
+const MarkAsFavorite = async (
+  mediaType: string,
+  mediaId: number,
+  favorite: boolean,
+  accountId: number,
+  sessionId: number
+): Promise<ApiResponse<MediaList>> => {
+  try {
+    const response = await fetch(
+      `${baseUrl}/Account/${accountId}/mark_as_favorite/${mediaType}/${mediaId}?sessionId=${sessionId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(favorite),
+      }
+    );
+    const data = await response.json();
+
+    if (!response.ok) {
+      return {
+        status: response.status,
+        error: `HTTP error! Status: ${response.status}`,
+      };
+    }
+
+    return { status: 200, data };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      status: 400,
+      error: error instanceof Error ? error.message : "Unknown error",
+    };
+  }
+};
+
+const MarkWatchlist = async (
+  mediaType: string,
+  mediaId: number,
+  favorite: boolean,
+  accountId: number,
+  sessionId: number
+): Promise<ApiResponse<MediaList>> => {
+  try {
+    const response = await fetch(
+      `${baseUrl}/Account/${accountId}/mark_watchlist/${mediaType}/${mediaId}?sessionId=${sessionId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(favorite),
+      }
+    );
+    const data = await response.json();
+
+    if (!response.ok) {
+      return {
+        status: response.status,
+        error: `HTTP error! Status: ${response.status}`,
+      };
+    }
+
+    return { status: 200, data };
+  } catch (error) {
+    console.error("Error:", error);
+    return {
+      status: 400,
+      error: error instanceof Error ? error.message : "Unknown error",
+    };
+  }
+};
+
+export default {
+  GetRequestToken,
+  GetSessionId,
+  GetAccountDetails,
+  GetUserList,
+  MarkAsFavorite,
+  MarkWatchlist,
+};
