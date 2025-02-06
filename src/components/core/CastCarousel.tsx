9b1412ae3f1218184625030d4504c3b704428a9a
cavadosi <jpeque.moab@gmail.com>
2025-02-06 16:57:03 +0100

    Fix build errors

---
+++ src/components/core/CastCarousel.tsx
@@ -0,0 +1,26 @@
+import { People } from "@/types";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import CastCard from "@/components/core/CastCard";
+
+export default function CastCarousel({ cast }: { cast: People[] }) {
+  if (!cast) return null;
+
+  return (
+    <Carousel className="mt-2">
+      <CarouselContent className="py-2">
+        {cast.map((el) => (
+          <CarouselItem
+            key={el.id}
+            className="ml-4 basis-3/12 md:basis-2/12 lg:basis-1/8"
+          >
+            <CastCard person={el}/>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
