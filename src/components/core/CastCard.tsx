1f34b5aa0a2353c0ea886275b6752e02716e476c
cavadosi <jpeque.moab@gmail.com>
2025-02-25 18:29:53 +0100

    Feat (CastCard): Empty image state

--- src/components/core/CastCard.tsx
+++ src/components/core/CastCard.tsx
@@ -9,15 +9,22 @@ export default function CastCard({ person }: { person: People }) {
 
   return (
     <div className="flex flex-col items-center">
-      {!imageLoaded && <Skeleton className="w-full aspect-[2/3] rounded-lg" />}
-      <img
-        src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
-        alt={person.name}
-        className={`w-full rounded-lg transition-opacity duration-300 ${
-          imageLoaded ? "opacity-100" : "opacity-0 hidden"
-        }`}
-        onLoad={handleImageLoad}
-      />
+      {!imageLoaded && !person.profile_path && (
+        <div className="w-full aspect-[2/3] rounded-lg bg-muted/20 flex items-center justify-center text-muted-foreground">
+          No Image Available
+        </div>
+      )}
+      {!imageLoaded && person.profile_path && <Skeleton className="w-full aspect-[2/3] rounded-lg" />}
+      {person.profile_path && (
+        <img
+          src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
+          alt={person.name}
+          className={`w-full rounded-lg transition-opacity duration-300 ${
+            imageLoaded ? "opacity-100" : "opacity-0 hidden"
+          }`}
+          onLoad={handleImageLoad}
+        />
+      )}
       <p className="text-xs mt-4 text-center">{person.name}</p>
       <p className="text-xs mt-2 text-center text-muted-foreground">{person.character}</p>
     </div>
